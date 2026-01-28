b2417f5aec14b10fca89b0851d92a97084c1b9d4
Rory Kirtz <159177574+Rorykirtz@users.noreply.github.com>
2026-01-28 18:49:38 -0500

    fixed issue with distance calculation
    Fix degree_tracker function and update distance calculation.

--- rory.py
+++ rory.py
@@ -1,18 +1,22 @@
 encoder_1 = Encoder(brain.three_wire_port.a)
 
-def degree_tracker():
-    encoder_1.reset_position
+def degree_tracker(old_position):
     position = encoder_1.position(DEGREES)
+    position = position - old_position
     return position
 
 def distance_tracker():
     distance = 0
 # replace with actual circumference of the wheels
-    circumference = 0
+    ccircumference = 0
     i = 0
+    old_position = 0
     while i < 30:
-        degree_tracker(position_old)
+        degree_tracker(old_position)
         distance += position/distance*circumference
+        old_position = old-position + position 
         i+=1
+        brain.screen.print(distance)
     if i = 30:
         i=0
+
